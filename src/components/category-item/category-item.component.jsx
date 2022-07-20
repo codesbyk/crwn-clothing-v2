ebe73810ba547b2f67f8cee84be96a96f7e78564
ravupati <ravupati@Apples-MacBook-Pro.local>
2022-07-20 12:48:27 +0530

    Added Two new components for container and menu items

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+const CategoryItem = ({category}) => {
+    const {id, title, imageUrl} = category;
+    return (
+        <div className='category-container'>
+            {/* <img /> */}
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl}` }}></div>
+            <div className='category-body-container'>
+                <h2>{title.toLocaleUpperCase()}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default CategoryItem;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({categories}) => {
+    return (
+        <div className='categories-container'>
+            {categories.map((category) => (
+                <CategoryItem category={category} key={category.id}></CategoryItem>
+            ))}
+        </div>
+    )
+}
+
+
+export default Directory;
